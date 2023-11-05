6a384ec9e95c0c08ce5a65672283eef46321fa04
mubashirAlgoace <mubashir.siddiqui@algoace.com>
2023-11-05 22:07:37 +0500

    post ctrl done

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,3 +1,33 @@
 import express from "express";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getTimeLine,
+  reactPost,
+  updatePost,
+} from "../controller/postCtrl.js";
 
 export const router = express.Router();
+
+//create a post
+
+router.post("/", createPost);
+//update a post
+
+router.put("/:id", updatePost);
+//delete a post
+
+router.delete("/:id", deletePost);
+//like / dislike a post
+
+router.put("/:id/like", reactPost);
+//get a post
+
+router.get("/:id", getPost);
+
+//get timeline posts
+
+router.get("/timeline/all", getTimeLine);
+
+module.exports = router;
