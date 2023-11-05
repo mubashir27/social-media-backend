67006548ffc38a8a5dde82cd431121b293b124f2
mubashirAlgoace <mubashir.siddiqui@algoace.com>
2023-11-05 20:50:29 +0500

    user with following and un following features done

---
+++ src/controller/userCtrl.js
@@ -0,0 +1,81 @@
+import User from "../models/User.js";
+
+const updateUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+};
+
+const deleteUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const followUser = async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("you already follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant follow yourself");
+  }
+};
+
+const unFollowUser = async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("user has been unFollowed");
+      } else {
+        res.status(403).json("you don't follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant un follow yourself");
+  }
+};
+
+export { updateUser, deleteUser, getUser, followUser, unFollowUser };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,3 +1,27 @@
 import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  followUser,
+  unFollowUser,
+} from "../controller/userCtrl.js";
 
 export const router = express.Router();
+
+//update user
+router.put("/:id", updateUser);
+
+//delete user
+router.delete("/:id", deleteUser);
+
+//get a user
+router.get("/:id", getUser);
+
+//follow a user
+
+router.put("/:id/follow", followUser);
+
+//unFollow a user
+
+router.put("/:id/unfollow", unFollowUser);
